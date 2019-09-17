b26a090d5311bea7b395f74085849fdf3c4d4488
ramiz-rahman <ramiz.rahman@outlook.com>
2019-09-18 00:41:56 +0600

    feat: Retrieve comments of a particular post

--- src/utils/PostsAPI.js
+++ src/utils/PostsAPI.js
@@ -93,3 +93,52 @@ export const vote = (postId, vote) => {
     .then((res) => res.json)
     .then((post) => post.json);
 };
+
+// COMMENTS
+
+// GET /posts/:id/comments
+// USAGE:
+//   Get all the comments for a single post
+
+export const getPostComments = (postId) => {
+  return fetch(`${api}/posts/${postId}/comments`, { headers })
+    .then((res) => {
+      return res.json();
+    })
+    .then((comments) => {
+      return comments;
+    });
+};
+
+// POST /comments
+// USAGE:
+//   Add a comment to a post
+
+// PARAMS:
+//   id: Any unique ID. As with posts, UUID is probably the best here.
+//   timestamp: timestamp. Get this however you want.
+//   body: String
+//   author: String
+//   parentId: Should match a post id in the database.
+
+// GET /comments/:id
+// USAGE:
+//   Get the details for a single comment
+
+// POST /comments/:id
+// USAGE:
+//   Used for voting on a comment.
+// PARAMS:
+//   option - String: Either "upVote" or "downVote"
+
+// PUT /comments/:id
+// USAGE:
+//   Edit the details of an existing comment
+
+// PARAMS:
+//   timestamp: timestamp. Get this however you want.
+//   body: String
+
+// DELETE /comments/:id
+// USAGE:
+//   Sets a comment's deleted flag to 'true'
