6d37a44728abf7758099df6657e30c17fa5d9e3d
ramiz-rahman <ramiz.rahman@outlook.com>
2019-10-01 01:42:25 +0600

    feat: Create modular Button Component
    Refactor the button component to be more modular so that
    it can be reused more often. The button now takes in the
    following optional props:
    
    * size: PropTypes.oneOf(['inline', 's', 'm', 'l', 'block'])
    * color: PropTypes.oneOf(['success', 'warning', 'danger']),
    * customStyles: PropTypes.object,
    
    The customStyles optional props is used to pass in a
    CSS-in-JS style object to customize the button beyond the
    limitations of the size and color props.

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,18 +2,60 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Button.module.css';
 
+function _enhanceClassName(className, enhancer) {
+  return `${className} ${enhancer}`;
+}
+
+function addSize(className, size) {
+  if (size === 'inline') {
+    return _enhanceClassName(className, styles.Button_size_inline);
+  } else if (size === 's') {
+    return _enhanceClassName(className, styles.Button_size_s);
+  } else if (size === 'm') {
+    return _enhanceClassName(className, styles.Button_size_m);
+  } else if (size === 'l') {
+    return _enhanceClassName(className, styles.Button_size_l);
+  } else if (size === 'block') {
+    return _enhanceClassName(className, styles.Button_size_block);
+  } else {
+    return className;
+  }
+}
+
+function addColor(className, color) {
+  if (color === 'success') {
+    return _enhanceClassName(className, styles.Button_color_success);
+  } else if (color === 'warning') {
+    return _enhanceClassName(className, styles.Button_color_warning);
+  } else if (color === 'danger') {
+    return _enhanceClassName(className, styles.Button_color_danger);
+  } else {
+    return className;
+  }
+}
+
 function Button(props) {
-  const { Icon, value, onClick } = props;
+  const { Icon, value, onClick, size, color, customStyles } = props;
+  let classNames = styles.Button;
+  classNames = addColor(classNames, color);
+  classNames = addSize(classNames, size);
   return (
-    <button onClick={onClick} className={styles.Button}>
+    <button
+      onClick={onClick}
+      className={classNames}
+      style={customStyles}
+    >
       {Icon && <Icon className={styles.Button__Icon} />}
-      <span>{value}</span>
+      <span className={styles.Button__Text}>{value}</span>
     </button>
   );
 }
 
 Button.propTypes = {
   Icon: PropTypes.func,
+  size: PropTypes.oneOf(['inline', 's', 'm', 'l', 'block']),
+  color: PropTypes.oneOf(['success', 'warning', 'danger']),
+  customStyles: PropTypes.object,
   value: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired
 };
