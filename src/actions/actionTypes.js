849e77386a19fc579401f05ca805985915e03287
ramiz-rahman <ramiz.rahman@outlook.com>
2019-10-02 16:09:08 +0600

    style: Match names in action types
    The identifier name (constant) and value name (string) of
    action types should be the same.
    
    example:
    UPDATE_POST = 'UPDATE_POST' rather than
    UPDATE_POST = 'EDIT_POST'

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -3,7 +3,7 @@ export const GET_CATEGORIES = 'GET_CATEGORIES';
 export const GET_ALL_POSTS = 'GET_ALL_POSTS';
 export const GET_POST = 'GET_POST';
 export const CREATE_POST = 'ADD_POST';
-export const UPDATE_POST = 'EDIT_POST';
+export const UPDATE_POST = 'UPDATE_POST';
 export const DELETE_POST = 'DELETE_POST';
 
 export const GET_POST_COMMENTS = 'GET_POST_COMMENTS';
